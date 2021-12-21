b8db278d0d79312402dd8813e07b3354e9fa468a
Aleksander M <aleksander.michalski222@gmail.com>
2021-12-21 09:43:11 +0100

    Initial commit

---
+++ src/pl/akademia108/Main.java
@@ -0,0 +1,13 @@
+package pl.akademia108;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float kurs = 4.09f;
+        float kurs100 = kurs * 100;
+
+        System.out.println(kurs);
+        System.out.println(kurs100);
+    }
+}
